fad1bd84f22c436211a129141a4bba4a7e47c83a
Antonello Di Nunzio <plugin@antonellodinunzio.online>
2022-02-23 15:14:39 +0100

    fix(dry): clean code

--- ladybug_geojson/to_geometry.py
+++ ladybug_geojson/to_geometry.py
@@ -25,6 +25,14 @@ except ImportError as e:
 
 COORDINATES = 'coordinates'
 
+def _get_coordinates(json_string: str):
+    validator = _Validator(json_string)
+    if not validator.is_valid:
+        return
+
+    obj = json.loads(json_string)
+    arr = obj.get(COORDINATES)
+    return arr
 
 '''____________2D GEOMETRY TRANSLATORS____________'''
 
@@ -34,12 +42,10 @@ def to_vector2d(json_string: str) -> Vector2D:
     Args:
         json_string: GEOJSON geometry string to translate
     '''
-    validator = _Validator(json_string)
-    if not validator.is_valid:
+    arr = _get_coordinates(json_string)
+    if not arr:
         return
 
-    obj = json.loads(json_string)
-    arr = obj.get(COORDINATES)
     return Vector2D.from_array(arr)
 
 def to_point2d(json_string: str) -> Point2D:
@@ -48,12 +54,10 @@ def to_point2d(json_string: str) -> Point2D:
     Args:
         json_string: GEOJSON geometry string to translate
     '''
-    validator = _Validator(json_string)
-    if not validator.is_valid:
+    arr = _get_coordinates(json_string)
+    if not arr:
         return
-
-    obj = json.loads(json_string)
-    arr = obj.get(COORDINATES)
+    
     return Point2D.from_array(arr)
 
 def to_linesegment2d(json_string: str) -> LineSegment2D:
@@ -62,12 +66,9 @@ def to_linesegment2d(json_string: str) -> LineSegment2D:
     Args:
         json_string: GEOJSON geometry string to translate
     '''
-    validator = _Validator(json_string)
-    if not validator.is_valid:
+    arr = _get_coordinates(json_string)
+    if not arr:
         return
-    
-    obj = json.loads(json_string)
-    arr = obj.get(COORDINATES)
 
     if len(arr) > 2:
         arr =  arr[::len(arr)-1]
@@ -84,12 +85,9 @@ def to_polyline2d(json_string: str,
         json_string: GEOJSON geometry string to translate
         interpolated: set it to true to create smooth polylines
     '''
-    validator = _Validator(json_string)
-    if not validator.is_valid:
+    arr = _get_coordinates(json_string)
+    if not arr:
         return
-    
-    obj = json.loads(json_string)
-    arr = obj.get(COORDINATES)
 
     if len(arr) == 2:
         return LineSegment2D.from_array(arr)
@@ -106,12 +104,10 @@ def to_vector3d(json_string: str) -> Vector3D:
     Args:
         json_string: GEOJSON geometry string to translate
     '''
-    validator = _Validator(json_string)
-    if not validator.is_valid:
+    arr = _get_coordinates(json_string)
+    if not arr:
         return
-
-    obj = json.loads(json_string)
-    arr = obj.get(COORDINATES)
+    
     return Vector3D.from_array(arr)
 
 def to_point3d(json_string: str) -> Point3D:
@@ -120,12 +116,10 @@ def to_point3d(json_string: str) -> Point3D:
     Args:
         json_string: GEOJSON geometry string to translate
     '''
-    validator = _Validator(json_string)
-    if not validator.is_valid:
+    arr = _get_coordinates(json_string)
+    if not arr:
         return
 
-    obj = json.loads(json_string)
-    arr = obj.get(COORDINATES)
     return Point3D.from_array(arr)
 
 
@@ -135,12 +129,9 @@ def to_linesegment3d(json_string: str) -> LineSegment3D:
     Args:
         json_string: GEOJSON geometry string to translate
     '''
-    validator = _Validator(json_string)
-    if not validator.is_valid:
+    arr = _get_coordinates(json_string)
+    if not arr:
         return
-    
-    obj = json.loads(json_string)
-    arr = obj.get(COORDINATES)
 
     if len(arr) > 2:
         arr =  arr[::len(arr)-1]
@@ -157,12 +148,9 @@ def to_polyline3d(json_string: str,
         json_string: GEOJSON geometry string to translate
         interpolated: set it to true to create smooth polylines
     '''
-    validator = _Validator(json_string)
-    if not validator.is_valid:
+    arr = _get_coordinates(json_string)
+    if not arr:
         return
-    
-    obj = json.loads(json_string)
-    arr = obj.get(COORDINATES)
 
     if len(arr) == 2:
         return LineSegment3D.from_array(arr)
